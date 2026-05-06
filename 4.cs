c6450b9e5006b3b8a44f3a753ce5739127fa2a53
Autumn May Valley <draqoshaimposter@gmail.com>
2026-05-06 10:54:48 +0300

    Replace last 3 characters of words with length 5

---
+++ 4.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] words = { "hello", "world", "cat", "amazing" };
+        int chosenLength = 5;
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length == chosenLength)
+            {
+                words[i] = words[i].Substring(0, words[i].Length - 3) + "$$$";
+            }
+        }
+
+        Console.WriteLine(string.Join(" ", words));
+    }
+}
+
+// ts was fun to make btw lol

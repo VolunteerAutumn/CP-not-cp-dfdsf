c37cae42ee071c7bdaeade89c81f860a1c57daa2
Autumn May Valley <draqoshaimposter@gmail.com>
2026-05-08 16:25:49 +0300

    Implement multithreaded statistics calculation

---
+++ threadhw/5.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Threading;
+
+
+class Program
+{
+    static void Main()
+    {
+        Random rnd = new Random();
+        int[] ints = new int[10000];
+        for (int i = 0; i <= 10000; i++)
+        {
+            ints[i] = rnd.Next(1, 100000);
+        }
+        void FindMax(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine("Max: " + max);
+        }
+        void FindMin(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine("Min: " + min);
+        }
+        void Average(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            Console.WriteLine("Average: " + (sum / arr.Length));
+        }
+        void Print(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        Thread max = new Thread(() => FindMax(ints));
+        Thread min = new Thread(() => FindMin(ints));
+        Thread avg = new Thread(() => Average(ints));
+        Thread print = new Thread(() => Print(ints));
+
+        print.Start();
+        max.Start();
+        min.Start();
+        avg.Start();
+
+    }
+}
